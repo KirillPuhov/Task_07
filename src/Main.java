04883a1c6cdc427b8fde6a31bdbb3175080ff8ce
KirillPuhov <dragonsuper1234509876@gmail.com>
2022-12-04 12:39:49 +0300

    Task_07_fix

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,42 @@
 //подпоследовательность полностью различных (ни один элемент не равен накакому другому элементу) подряд идущих элементов массива
 //(реализовать функцию, которая будет возвращать позицию первого элемента такой подпоследовательности и кол-во элементов).
 
-public class Main {
-    public static void main(String[] args) {
-        int[] array1 = {1,2,3,4,5,5,4,77,2,12,3,3,3,5,6,7,8,8};
-        int[] array2 = {1,2,3,4,15,15,53,65,54,10,0,29,8,8};
-        int[] array3 = {1,1};
-        int[] array4 = {1,2,1,2,1,2};
-        int[] array5 = {0,0,0,0,1,1,1,1,2,3,4,5};
-        int[] array6 = {0,1,0,2,0,3,0,4,0,5,0,6};
-        int[] array7 = {1,2,3,4,5,1,1,1,1,6,7,8};
-        int[] array8 = {0,1,3,4,5,6,7,8,9};
-        int[] array9 = {2,2,2,1};
-        int[] array10 = {1,2,3,2,2};
+import java.util.Random;
+import java.util.Scanner;
 
-        solution(array1);
-        solution(array2);
-        solution(array3);
-        solution(array4);
-        solution(array5);
-        solution(array6);
-        solution(array7);
-        solution(array8);
-        solution(array9);
-        solution(array10);
+public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        tests();
+        randomArrQuestion();
     }
+
+    private static void tests(){
+        solution(new int[]{1,2,3,4,5,5,4,77,2,12,3,3,3,5,6,7,8,8});
+        solution(new int[]{1,2,3,4,15,15,53,65,54,10,0,29,8,8});
+        solution(new int[]{1,1});
+        solution(new int[]{1,2,1,2,1,2});
+        solution(new int[]{0,0,0,0,1,1,1,1,2,3,4,5});
+        solution(new int[]{0,1,0,2,0,3,0,4,0,5,0,6});
+        solution(new int[]{1,2,3,4,5,1,1,1,1,6,7,8});
+        solution(new int[]{0,1,3,4,5,6,7,8,9});
+        solution(new int[]{2,2,2,1});
+        solution(new int[]{1,2,3,2,2});
+    }
+
+    private static void randomArrQuestion(){
+        System.out.println("Задать произвольный массив в качестве входных данных?");
+        String answer = scan.nextLine().toUpperCase();
+
+        if(answer.equals("YES"))
+            solution(randomizeArray());
+    }
+
     private static void solution(int[] arr){
-        int maxCounter = 0;
+        printArray(arr);
+
+        int maxLength = 0;
         int maxIndex = 0;
         int index = 0;
 
@@ -37,8 +47,8 @@ public class Main {
             indexI++;
             for(int j=index;j<i;j++){
                 if(arr[i] == arr[j]){
-                    if(maxCounter <= i-index){
-                        maxCounter = i-index;
+                    if(maxLength <= i-index){
+                        maxLength = i-index;
                         maxIndex = index;
                     }
                     index = i;
@@ -46,11 +56,32 @@ public class Main {
             }
         }
 
-        if(maxCounter <= indexI-index){
-            maxCounter = indexI-index;
+        if(maxLength <= indexI-index){
+            maxLength = indexI-index;
             maxIndex = index;
         }
 
-        System.out.println(maxCounter + " - " + maxIndex);
+        System.out.printf("Позиция первого элемента: %d; Кол-во элементов: %d\n", maxIndex, maxLength);
+        System.out.println("------------------------------------------------");
+    }
+
+    private static int[] randomizeArray(){
+        var rand = new Random();
+        int size = rand.nextInt(50);
+
+        int[] array = new int[size];
+
+        for(int i=0; i < size; i++){
+            array[i] = rand.nextInt(200);
+        }
+
+        return array;
+    }
+
+    private static void printArray(int[] arr){
+        for (int j : arr) {
+            System.out.printf("%d\s", j);
+        }
+        System.out.println();
     }
 }
